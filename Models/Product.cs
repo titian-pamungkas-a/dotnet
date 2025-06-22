3827e5b4bc2e84aa6e446b9589eea4963b3fde8f
titian-pamungkas-a <pamungkastitian@gmail.com>
2025-06-22 20:00:17 +0700

    .

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EmptyProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ namespace EmptyProject
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            //builder.Services.AddOpenApi();
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
